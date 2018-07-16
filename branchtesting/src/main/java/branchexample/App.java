25d9d9ec3b8a3d5c7666bb56aefad84e84ba3569
Brian Cipollone <brian.cipollone@sonarsource.com>
2018-07-16 16:22:48 -0500

    Issues introduced

--- branchtesting/src/main/java/branchexample/App.java
+++ branchtesting/src/main/java/branchexample/App.java
@@ -4,7 +4,12 @@ public class App
 {
     public void aMethod( String[] args )
     {
-    	
+    	// TODO fix this
+    	int i = 0;
+    	if (i == 0) {
+    		// TODO fix this too..
+    		int j = 5 / i;
+    	}
     	System.out.println("IN AMETHOD"); 
     }
     
